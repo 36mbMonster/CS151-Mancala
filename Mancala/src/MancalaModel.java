4df5047a816202711686906acfa079c5884c5527
Jon Vaccaro <je.jonathan@gmail.com>
2013-05-04 09:45:13 -0700

    Fixed bug where circular list was not initialized to be circular. Minor other improvements to move code.

--- Mancala/src/MancalaModel.java
+++ Mancala/src/MancalaModel.java
@@ -4,7 +4,6 @@ public class MancalaModel
 	public static final int NUM_PLAYERS = 2;
 	public static final int NUM_MANCALAS = 2;
 	public static final int TOTAL_INDECIES = (NUM_PITS * NUM_PLAYERS) + NUM_MANCALAS;
-	public static final int MAX_STONES_PER_PIT = 4;
 	public static final int MAX_UNDOS = 3;
 	
 	private Pit[] board;
@@ -17,13 +16,15 @@ public class MancalaModel
 		private Pit next;
 		private int pieces;
 		private int player;
+		private int index;
 		private boolean mancala;
 		
-		public Pit(int pieces, boolean mancala, int player)
+		public Pit(int pieces, boolean mancala, int index, int player)
 		{
 			this.mancala = mancala;
 			this.pieces = pieces;
 			this.player = player;
+			this.index = index;
 		}
 		
 		public int getPieces()
@@ -34,6 +35,11 @@ public class MancalaModel
 		public int getPlayer()
 		{
 			return player;
+		}
+		
+		public int getIndex()
+		{
+			return index;
 		}
 		
 		public void setNext(Pit next)
@@ -64,6 +70,7 @@ public class MancalaModel
 	
 	public MancalaModel(int startingNumPieces)
 	{
+		System.out.println(TOTAL_INDECIES);
 		board = new Pit[TOTAL_INDECIES];
 		potentialMove = new Pit[NUM_PITS];
 		turnUndosLeft = MAX_UNDOS;
@@ -72,16 +79,25 @@ public class MancalaModel
 		
 		//Player1
 		for(int i = 0; i < TOTAL_INDECIES/2; i++)
-			board[i] = new Pit(startingNumPieces,false,0);
-		for(int i = 0; i < TOTAL_INDECIES/2; i++)
-			board[i] = new Pit(startingNumPieces,false,0);
+			board[i] = new Pit(startingNumPieces,false,i,0);
 		//Player2
 		for(int i = TOTAL_INDECIES/2; i < TOTAL_INDECIES; i++)
-			board[i] = new Pit(startingNumPieces,false,1);
+			board[i] = new Pit(startingNumPieces,false,i,1);
 		
 		//The mancalas
-		board[6] = new Pit(startingNumPieces,true,0);
-		board[13] = new Pit(startingNumPieces,true,1);
+		board[6] = new Pit(0,true,6,0);
+		board[13] = new Pit(0,true,13,1);		
+		
+		//link the pits together.
+		for(int i = 0; i < TOTAL_INDECIES; i++)
+		{
+			//When the last pit is reached, link it to the first pit.
+			if(i + 1 == TOTAL_INDECIES)
+				board[i].setNext(board[0]);
+			else
+				board[i].setNext(board[i + 1]);
+		}
+	
 			
 	}
 	
@@ -95,22 +111,23 @@ public class MancalaModel
 		if(player == 0) playerMancala = 6;
 		else playerMancala = 13;
 		
-		int currentIndex = startIndex;
+		//Reference to current pit. Used for cycling through the circular list.
+		Pit currentPit = potentialMove[startIndex]; 
 		for(int i = 1; i < potentialMove[startIndex].getPieces() + 1; i++)
 		{
-			currentIndex = startIndex + i; 
-			potentialMove[currentIndex].addPieces(1);
+			currentPit = currentPit.getNext(); 
+			currentPit.addPieces(1);
 		}
 		
 		//If your last stone lands in your mancala, free turn
-		if(potentialMove[currentIndex].isMancala() && potentialMove[currentIndex].getPlayer() == player)
+		if(currentPit.isMancala() && currentPit.getPlayer() == player)
 			return true;
 		
 		//If your last stone lands in an empty pit on your side, take all stones from the pit opposite of it
 		//and put it in your mancala
-		else if(potentialMove[currentIndex].getPieces() == 0 && potentialMove[currentIndex].getPlayer() == player)
+		else if(currentPit.getPieces() == 0 && currentPit.getPlayer() == player)
 		{
-			int oppositeIndex = (13 - currentIndex - 1);
+			int oppositeIndex = (13 - currentPit.getIndex() - 1);
 			potentialMove[playerMancala].addPieces(potentialMove[oppositeIndex].getPieces());
 			potentialMove[oppositeIndex].setPieces(0);
 			return false;
@@ -132,6 +149,11 @@ public class MancalaModel
 		}
 		else
 			return false;
+	}
+	
+	public int getPiecesInPit(int index)
+	{
+		return board[index].getPieces();
 	}
 	
 	//Set the board to the potential move to end the turn.
