5e91994e4f07cc85ab41d7fd4ec65702f061bdfe
Jon Vaccaro <je.jonathan@gmail.com>
2013-05-04 17:36:30 -0700

    Finally finished debugging and testing the model. I just need to add change listener stuff now.

--- Mancala/src/MancalaModel.java
+++ Mancala/src/MancalaModel.java
@@ -9,6 +9,8 @@ public class MancalaModel
 	private Pit[] board;
 	private Pit[] potentialMove;
 	private int turnUndosLeft;
+	private int currentTurn;
+	private boolean freeTurn;
 	
 	//Node class for circular list structure
 	public class Pit
@@ -68,12 +70,15 @@ public class MancalaModel
 		}
 	}
 	
-	public MancalaModel(int startingNumPieces)
+	public MancalaModel(int startingNumPieces, int startingTurn)
 	{
-		System.out.println(TOTAL_INDECIES);
 		board = new Pit[TOTAL_INDECIES];
 		potentialMove = new Pit[NUM_PITS];
 		turnUndosLeft = MAX_UNDOS;
+		freeTurn = false;
+		
+		//Set who's turn it is on the first round.
+		currentTurn = startingTurn;
 		
 		//Initialize board
 		
@@ -102,10 +107,12 @@ public class MancalaModel
 	}
 	
 	//handle zero pieces in pit error on interface side.
-	//returns true if free turn.
+	//returns true if free move successful.
 	public boolean move(int startIndex, int player)
 	{
-		potentialMove = board;
+
+		potentialMove = new Pit[TOTAL_INDECIES];
+		copyBoard(board, potentialMove);
 		int playerMancala;
 		
 		if(player == 0) playerMancala = 6;
@@ -121,27 +128,36 @@ public class MancalaModel
 		
 		//If your last stone lands in your mancala, free turn
 		if(currentPit.isMancala() && currentPit.getPlayer() == player)
+		{
+			potentialMove[startIndex].setPieces(0);
+			freeTurn = true;
 			return true;
+		}
 		
 		//If your last stone lands in an empty pit on your side, take all stones from the pit opposite of it
 		//and put it in your mancala
-		else if(currentPit.getPieces() == 0 && currentPit.getPlayer() == player)
+		else if(currentPit.getPieces() == 1 && currentPit.getPlayer() == player)
 		{
+			//Capture the opposite pit's pieces
 			int oppositeIndex = (13 - currentPit.getIndex() - 1);
 			potentialMove[playerMancala].addPieces(potentialMove[oppositeIndex].getPieces());
 			potentialMove[oppositeIndex].setPieces(0);
-			return false;
+			
+			//put the piece you dropped in the mancala as well.
+			potentialMove[playerMancala].addPieces(currentPit.getPieces());
+			currentPit.setPieces(0);
+			return true;
 		}
 		//If your last stone lands in an opponent's pit, your turn ends.
 		potentialMove[startIndex].setPieces(0);
-		return false;
+		return true;
 		
 	}
 	
 	//Returns true if undos are available and false if they aren't
 	public boolean undo()
 	{
-		if(turnUndosLeft > 0)
+		if(turnUndosLeft > 0 && potentialMove != board)
 		{
 			potentialMove = board;
 			turnUndosLeft--;
@@ -151,14 +167,79 @@ public class MancalaModel
 			return false;
 	}
 	
+	public boolean isMancala(int index)
+	{
+		return board[index].isMancala();
+	}
+	
+	//Is the game finished? Did someone win?
+	public boolean gameOver()
+	{
+		boolean zeroCount1 = true;
+		boolean zeroCount2 = true;
+		//Check if player1 has any pieces left in their pits
+		for(int i = 0; i < TOTAL_INDECIES/2 - 1; i++)
+			if(board[i].getPieces() != 0) zeroCount1 = false;
+		//Check if player2 has any pieces left in their pits
+		for(int i = TOTAL_INDECIES/2 - 1; i < TOTAL_INDECIES - 1; i++)
+			if(board[i].getPieces() != 0) zeroCount2 = false;
+		
+		return zeroCount1 || zeroCount2;			
+			
+	}
+	
 	public int getPiecesInPit(int index)
 	{
 		return board[index].getPieces();
 	}
 	
+	public int getTotalPlayerPieces(int player)
+	{
+		int total = 0;
+		for(int i = 0; i < TOTAL_INDECIES; i++)
+		{
+			if(board[i].getPlayer() == player && !board[i].isMancala()) 
+				total += board[i].getPieces();
+		}
+		return total;
+	}
+	
+	public int getAvailableUndos()
+	{
+		return turnUndosLeft;
+	}
+	
+	public int getPlayerTurn()
+	{
+		return currentTurn;
+	}
+	
 	//Set the board to the potential move to end the turn.
 	public void endTurn()
 	{
-		board = potentialMove;
+		copyBoard(potentialMove, board);
+		if(!freeTurn)currentTurn ^= 1; //bit xor with one to change who's turn it is. 
+		else freeTurn = false;
+	}
+	
+	//Helper method to copy one array to another, so it's not just a reference to the other board.
+	private void copyBoard(Pit[] from, Pit[] to)
+	{
+	
+		for(int i = 0; i < TOTAL_INDECIES; i++)
+			to[i] = new Pit(from[i].getPieces(), from[i].isMancala(), from[i].getIndex(), from[i].getPlayer());
+		
+		to[6] = new Pit(from[6].getPieces(),true,6,0);
+		to[13] = new Pit(from[13].getPieces(),true,13,1);	
+		
+		//link the pits together.
+		for(int i = 0; i < TOTAL_INDECIES; i++)
+		{
+			//When the last pit is reached, link it to the first pit.
+			if(i + 1 == TOTAL_INDECIES)
+				to[i].setNext(to[0]);
+			else
+				to[i].setNext(to[i + 1]);
+		}
 	}
 }
--- Mancala/src/modelTest.java
+++ Mancala/src/modelTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class modelTest 
 {
-	MancalaModel boardModel = new MancalaModel(4);
+	MancalaModel boardModel = new MancalaModel(4,0);
 	
 	@Test
 	public void moveTest() 
@@ -24,6 +24,112 @@ public class modelTest
 		assertEquals(5, boardModel.getPiecesInPit(11));
 		assertEquals(5, boardModel.getPiecesInPit(12));
 		assertEquals(1, boardModel.getPiecesInPit(13));
+		assertEquals(1, boardModel.getPiecesInPit(0));
+		
+		boardModel.move(5,0);
+		boardModel.endTurn();
+		assertEquals(0, boardModel.getPiecesInPit(5));
+		assertEquals(1, boardModel.getPiecesInPit(6));
+		assertEquals(5, boardModel.getPiecesInPit(7));
+		assertEquals(5, boardModel.getPiecesInPit(8));
+		assertEquals(5, boardModel.getPiecesInPit(9));
+		
+		boardModel.move(11,1);
+		boardModel.endTurn();
+		assertEquals(0, boardModel.getPiecesInPit(11));
+		assertEquals(6, boardModel.getPiecesInPit(12));
+		assertEquals(2, boardModel.getPiecesInPit(13));
+		assertEquals(2, boardModel.getPiecesInPit(0));
+		assertEquals(6, boardModel.getPiecesInPit(1));
+		assertEquals(6, boardModel.getPiecesInPit(2));
+		
+	}
+	
+	@Test
+	public void captureTest()
+	{
+		boardModel = new MancalaModel(4,0);
+		boardModel.move(4, 0);
+		boardModel.endTurn();
+		boardModel.move(0, 0);
+		boardModel.endTurn();
+		assertEquals(7, boardModel.getPiecesInPit(6));
+		
+		//Check to make sure it doesn't try to capture unless last move.
+		boardModel.move(7, 1);
+		boardModel.endTurn();
+		assertEquals(1, boardModel.getPiecesInPit(8));
+		assertEquals(0, boardModel.getPiecesInPit(13));
+		
+	}
+	
+	@Test
+	public void undoTest()
+	{
+		boardModel = new MancalaModel(4,0);
+		
+		assertEquals(3, boardModel.getAvailableUndos());
+		boardModel.move(4, 0);
+		boardModel.undo();
+		assertEquals(2, boardModel.getAvailableUndos());
+		boardModel.move(4, 0);
+		boardModel.undo();
+		assertEquals(1, boardModel.getAvailableUndos());
+		boardModel.move(4, 0);
+		boardModel.undo();
+		assertEquals(0, boardModel.getAvailableUndos());
+		
+		//Check all pits and mancalas to make sure nothing changed.
+		for(int i = 0; i < MancalaModel.TOTAL_INDECIES; i++)
+		{
+			//System.out.println("INDEX "+i+": "+boardModel.getPiecesInPit(i));
+			if(!boardModel.isMancala(i))
+				assertEquals(4, boardModel.getPiecesInPit(i));
+			else
+				assertEquals(0, boardModel.getPiecesInPit(i));
+		}
+	}
+	
+	@Test
+	public void freeTurnTest()
+	{
+		boardModel = new MancalaModel(4,0);
+		
+		assertEquals(0, boardModel.getPlayerTurn());
+		boardModel.move(2, 0);
+		boardModel.endTurn();
+		assertEquals(0, boardModel.getPlayerTurn());
+		boardModel.move(3, 0);
+		boardModel.endTurn();
+		assertEquals(1, boardModel.getPlayerTurn());
+		
+	}
+	
+	@Test
+	public void winTest()
+	{
+		boardModel = new MancalaModel(4,0);
+		
+		boardModel.move(0, 0);
+		boardModel.endTurn();		
+		boardModel.move(1, 0);
+		boardModel.endTurn();
+		boardModel.move(2, 0);
+		boardModel.endTurn();
+		boardModel.move(3, 0);
+		boardModel.endTurn();
+		boardModel.move(4, 0);
+		boardModel.endTurn();
+		boardModel.move(5, 0);
+		boardModel.endTurn();
+		assertEquals(0, boardModel.getPiecesInPit(0));
+		assertEquals(0, boardModel.getPiecesInPit(1));
+		assertEquals(0, boardModel.getPiecesInPit(2));
+		assertEquals(0, boardModel.getPiecesInPit(3));
+		assertEquals(0, boardModel.getPiecesInPit(4));
+		assertEquals(0, boardModel.getPiecesInPit(5));
+		
+		assertEquals(true, boardModel.gameOver());
 	}
 
 }
